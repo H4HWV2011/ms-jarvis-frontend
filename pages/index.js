d62ed6f2b840bcd3ee68be79008b16b8c865e2a4
Carrie Kidd <h4hwv2011@gmail.com>
2025-07-29 19:02:08 -0400

    FIX: remove old dashboard index.js, homepage is now Ms.Jarvis chat

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-// pages/index.js - Testing Framework Dashboard
-import TestingFrameworkDashboard from '../components/TestingFrameworkDashboard';
-
-export default function Home() {
-  return <TestingFrameworkDashboard />;
-}
